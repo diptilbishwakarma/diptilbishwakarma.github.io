2bee0954f50bed81f51977e3c0bf9ade17974bae
Diptil Bishwakarma <diptilbishwakarma@Diptils-MacBook-Pro.local>
2025-01-30 00:40:40 -0700

    Added footer for the copyright

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 function setDark() {
     document.body.classList.remove('light_mode');
     document.body.classList.add('dark_mode');
-    document.getElementsByClassName('stats_theme').innerHTML = "Current Page Theme: Light";
+    document.getElementById('stats_theme').innerHTML = "Current Page Theme: Dark";
 }
 
 function setLight() {
     document.body.classList.remove('dark_mode');
     document.body.classList.add('light_mode');
-    document.getElementsByClassName('stats_theme').innerHTML = "Current Page Theme: Dark";
+    document.getElementById('stats_theme').innerHTML = "Current Page Theme: Light";
 }
 
 function setDefault() {
     document.body.classList.remove('dark_mode');
     document.body.classList.remove('light_mode');
-    document.getElementsByClassName('stats_theme').innerHTML = "Current Page Theme: Default";
+    document.getElementById('stats_theme').innerHTML = "Current Page Theme: Default";
 }
 
 function perimeter() {
